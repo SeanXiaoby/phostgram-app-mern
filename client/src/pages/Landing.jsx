9fead2afa28f5119c5b0bd0048d14b265cc8a85a
Boyang Xiao <sean_xiaoby@163.com>
2023-04-30 15:42:46 -0700

    landing page fixed hook bug

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -5,8 +5,7 @@ import SigninForm from "../components/SigninForm";
 import SignupForm from "../components/SignupForm";
 
 const Landing = () => {
-  const { landingType, setLandingType } = useState("signin");
-
+  const [landingType, setLandingType] = useState("signin");
   const changeLandingType = (type) => {
     if (type === "signin" || type === "signup") {
       setLandingType(type);
