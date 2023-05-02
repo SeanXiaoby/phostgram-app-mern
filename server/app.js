eb61a4e0addd50f02efda9cf4ecef70b572377ca
Boyang Xiao <sean_xiaoby@163.com>
2023-05-01 23:55:37 -0700

    Updated mongodb endpoints

--- server/app.js
+++ server/app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const db = require("./models/db.js");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = 3001;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/ping", async (req, res, next) => {
+  res.status(204).send();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get("/user", async (req, res, next) => {
+  const info = req.body;
+  const user_id = await db.insertUser(info);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(200).send(user_id);
 });
+// Initiate the server
 
-module.exports = app;
+(async () => {
+  console.log("Service is initialized!");
+  console.log("Waiting for connecting to MongoDB...");
+
+  try {
+    await db.connectDB();
+  } catch (err) {
+    console.log(err);
+    process.exit(5);
+  }
+
+  console.log("MongoDB is sucessfully connected!");
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+  });
+})();
---
+++ server/models/db.js
@@ -0,0 +1,48 @@
+const { MongoClient, ObjectId } = require("mongodb");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+class dbAPI {
+  constructor() {
+    dotenv.config();
+    // let uri =
+    //   process.env.mongoSource === "atlas"
+    //     ? process.env.atlas_mongoURI
+    //     : process.env.local_mongoURI;
+    const uri =
+      "mongodb+srv://boyangxiao:1998629@cluster0.lfbgty1.mongodb.net/?retryWrites=true&w=majority&useUnifiedTopology=true";
+    console.log(process.env.mongoSource);
+    this._client = new MongoClient(uri);
+  }
+
+  async connectDB() {
+    try {
+      await this._client.connect();
+      await this._client.db("ee547_final").command({ ping: 1 });
+    } catch (err) {
+      throw err;
+    }
+
+    this._db = this._client.db("ee547_final");
+    this._users = this._db.collection("users");
+    this._log = this._db.collection("log");
+    this._phosts = this._db.collection("phosts");
+  }
+
+  async insertUser(info) {
+    const { insertedId: mid } = await this._users.insertOne({
+      username: "Sean",
+      password: "123456",
+    });
+    console.log(mid.toString());
+
+    const user = await this._users.findOne({ username: "Sean1" });
+
+    console.log(user);
+
+    return mid.toString();
+  }
+}
+
+const db = new dbAPI();
+module.exports = db;
