d1b4e79a2dd2d596010bf2c142cb3603d2b41a7a
Boyang Xiao <sean_xiaoby@163.com>
2023-05-03 14:12:00 -0700

    Init cloudinary based apis

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Phostlist from "../components/PhostList";
 import CreateBtn from "../components/CreateBtn";
 
 const Home = () => {
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("session_id") !== null
+  );
+
   return (
     <>
       <Navbar />
       <div className="page-content">
         <Phostlist />
       </div>
-      <CreateBtn />
+      {loggedIn && <CreateBtn />}
       <Footer />
     </>
   );
---
+++ client/src/utils/util_cloudinary.js
@@ -0,0 +1,9 @@
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//   cloud_name: "domiuzup5",
+//   api_key: "753663794727959",
+//   api_secret: "3vxSv5Ue3u1udWApE6NsUvDON5I",
+// });
+
+// export default cloudinary;
