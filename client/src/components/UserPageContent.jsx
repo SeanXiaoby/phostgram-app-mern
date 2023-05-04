9c6edfa4a14266b00c8d29f238ee610b9a6818c0
Boyang Xiao <sean_xiaoby@163.com>
2023-05-03 23:10:43 -0700

    Updated user page fetch data

--- client/src/components/UserPageContent.jsx
+++ client/src/components/UserPageContent.jsx
@@ -1,45 +1,91 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import default_avatar from "../img/default_avatar.jpg";
 import { sample_phosts, sample_users } from "../data/data";
 import { useNavigate } from "react-router-dom";
+import { serverInfo } from "../data/serverInfo";
 
 const UserPageContent = ({ user }) => {
   const [userData, setUserData] = useState(user);
-  const [userPhosts, setUserPhosts] = useState(sample_phosts);
+  const [userPhosts, setUserPhosts] = useState([]);
+  const [fetched, setFetched] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [failed, setFailed] = useState(false);
+
   const navigate = useNavigate();
 
   const handleClickImg = (phost_id) => {
     navigate(`/phost/${phost_id}`);
   };
 
+  const fetchUserPhosts = async (user_id) => {
+    if (user_id === undefined || user_id === null) {
+      return null;
+    }
+
+    try {
+      const response = await fetch(
+        serverInfo.url + `/api/user/${user_id}/phosts`
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setUserPhosts(data.phosts);
+        setFetched(true);
+        setLoading(false);
+      } else {
+        setFailed(true);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+      setFailed(true);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserPhosts(userData.id);
+  }, []);
+
   return (
     <div className="user-page-content">
       <div className="user-page-info">
         <img
-          src={userData.avatar === undefined ? default_avatar : userData.avatar}
+          src={
+            userData === null
+              ? default_avatar
+              : userData.avatar === null
+              ? default_avatar
+              : userData.avatar
+          }
           alt="avatar"
         />
-        <h4>
-          {userData.username === undefined ? "unknown" : userData.username}
-        </h4>
+        <h4>{userData === null ? "unknown" : userData.username}</h4>
       </div>
 
-      <div className="user-page-gallery">
-        {userPhosts.length ? (
-          userPhosts.map((phost) => {
-            return (
-              <img
-                src={phost.img}
-                alt="phost-img"
-                key={phost.id}
-                onClick={() => handleClickImg(phost.id)}
-              />
-            );
-          })
+      {loading && <div className="loading" />}
+
+      {!loading && failed && (
+        <div className="alert-danger">Failed to load data</div>
+      )}
+
+      {fetched &&
+        (userPhosts.length ? (
+          <div className="user-page-gallery">
+            {userPhosts.map((phost) => {
+              return (
+                <img
+                  src={phost.img}
+                  alt="phost-img"
+                  key={phost.id}
+                  onClick={() => handleClickImg(phost.id)}
+                />
+              );
+            })}
+          </div>
         ) : (
-          <h4>No phost yet...</h4>
-        )}
-      </div>
+          <h4 className="user-page-no-post">No phost yet...</h4>
+        ))}
     </div>
   );
 };
--- client/src/pages/Userpage.jsx
+++ client/src/pages/Userpage.jsx
@@ -1,17 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import UserpageContent from "../components/UserPageContent";
 import { sample_users } from "../data/data";
+import { serverInfo } from "../data/serverInfo";
+import { useParams } from "react-router";
 
 const Userpage = () => {
-  const [user, setUser] = useState(sample_users[1]);
+  const [user, setUser] = useState(null);
+  const [fetched, setFetched] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [failed, setFailed] = useState(false);
+
+  const user_id = useParams().id;
+
+  const fetchUserData = async (user_id) => {
+    if (user_id === undefined || user_id === null) {
+      return null;
+    }
+
+    try {
+      const response = await fetch(serverInfo.url + `/api/user/${user_id}`);
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setUser(data.user === undefined ? null : data.user);
+        setFetched(true);
+        setLoading(false);
+      } else {
+        setUser(null);
+        setFailed(true);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      setFailed(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData(user_id);
+  }, []);
 
   return (
     <>
       <Navbar />
       <div className="page-content">
-        <UserpageContent user={user} />
+        {fetched && <UserpageContent user={user} />}
       </div>
       <Footer />
     </>
